eaca74a6653394130cf51fca98629fc3ed137703
eunju.kim <sb02348@hanmail.net>
2022-03-25 22:39:31 +0900

    Initial Setup

---
+++ src/components/Detail.js
@@ -0,0 +1,8 @@
+export default function Detail(){
+    return(
+        <article>
+            <h2>Book title</h2>
+            <h5>Book contents</h5>
+        </article>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return(
+        <nav>
+            <ol>
+                <li><a href="/read/1">Book1</a></li>
+                <li><a href="/read/2">Book2</a></li>
+                <li><a href="/read/3">Book3</a></li>
+            </ol>
+        </nav>
+    )
+}
